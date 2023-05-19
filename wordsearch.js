9bc636834caabe490c3af6b798e6799ed2535400
nahcg <glor.chan@mail.utoronto.ca>
2023-05-18 20:32:21 -0400

    initial commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
 const wordSearch = (letters, word) => { 
+    if (!word || word.length === 0) {
+        return false;
+    }
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
+
+    const verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++) {
+    let verticalString = '';
+    for (let j = 0; j < letters.length; j++) {
+      verticalString += letters[j][i];
+    }
+    verticalJoin.push(verticalString);
+  }
+
+    for (v of verticalJoin) {
+        if (v.includes(word)) {
+            return true;
+        }
+    }    
+    return false;
 }
 
+
 module.exports = wordSearch
+
+
+
+// in collaboration with SabaO7, nahcg, madiha-waqar
+  
